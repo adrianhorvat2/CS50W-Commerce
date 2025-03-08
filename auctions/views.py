ef689e429c7efb9568882ca1ffbbefff03e82597
adrianhorvat2 <adro.horvat@gmail.com>
2025-03-08 21:32:28 +0100

    Solve 3.task

--- auctions/views.py
+++ auctions/views.py
@@ -9,7 +9,12 @@ from .models import User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+
+    listings = Listing.objects.all()
+
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
