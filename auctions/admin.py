35fa3fd9512094518e641fb02eeb3130367c42dc
adrianhorvat2 <adro.horvat@gmail.com>
2025-03-09 17:33:00 +0100

    Change django admin UI for Listing

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Listing, Category, Bid, Comment
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'is_active', 'watching_users')  
+    filter_horizontal = ('watching',)  
+    def watching_users(self, obj):
+        return ", ".join([user.username for user in obj.watching.all()])
+    
+    watching_users.short_description = "Watching Users"
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category)
 admin.site.register(Bid)
 admin.site.register(Comment)
